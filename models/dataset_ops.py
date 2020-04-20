e01a90c7561e19697c1019912fd9fa9996af2866
Mohammad Jafar Mashhadi <raindigital2007@gmail.com>
2020-04-20 14:16:51 -0600

    Refactored the dataset_ops.py to suppord only word/only character vectorizations

--- models/dataset_ops.py
+++ models/dataset_ops.py
@@ -50,41 +50,77 @@ def load_data(*, file_name='../data/cleaned-and-combined.csv', split_ratio=None,
     return data_train, data_validation, data
 
 
-def create_dataset_preloaded(word_vectorizer, char_vectorizer, data: pd.DataFrame, vec_length=200):
-    word_tokenizer = word_vectorizer.build_tokenizer()
+def create_dataset_preloaded(word_vectorizer, char_vectorizer, data: pd.DataFrame, one_hot=False, vec_length=200):
+    assert word_vectorizer is not None or char_vectorizer is not None
 
-    # wv = tf.constant(postpad_to(word_vectorizer.texts_to_sequences(data['url']), vec_length), name='word')
-    wv = tf.constant(postpad_to(
-        data['url'].map(lambda url: [word_vectorizer.vocabulary_.get(a, -1)+2 for a in word_tokenizer(url)])  # 0 = padding, 1 = OOV
-        , vec_length), name='word')
-    cv = tf.constant(postpad_to(char_vectorizer.texts_to_sequences(data['url']), vec_length), name='char')
-    targets = tf.one_hot(data['type'], depth=2)
+    if word_vectorizer is not None:
+        word_tokenizer = word_vectorizer.build_tokenizer()
+        # wv = tf.constant(postpad_to(word_vectorizer.texts_to_sequences(data['url']), vec_length), name='word')
+        wv = tf.constant(postpad_to(
+            data['url'].map(lambda url: [word_vectorizer.vocabulary_.get(a, -1)+2 for a in word_tokenizer(url)])  # 0 = padding, 1 = OOV
+            , vec_length), name='word')
 
-    ds = tf.data.Dataset.from_tensor_slices(((wv, cv), targets))
+    if char_vectorizer is not None:
+        cv = tf.constant(postpad_to(char_vectorizer.texts_to_sequences(data['url']), vec_length), name='char')
+
+    if one_hot:
+        targets = tf.squeeze(tf.one_hot(data['type'], depth=2))
+    else:
+        targets = data['type']
+
+    if word_vectorizer is not None:
+        if char_vectorizer is not None:
+            ds = tf.data.Dataset.from_tensor_slices(((wv, cv), targets))
+        else:
+            ds = tf.data.Dataset.from_tensor_slices((wv, targets))
+    else:
+        ds = tf.data.Dataset.from_tensor_slices((cv, targets))
+
     return ds
 
 
-def create_dataset_generator(word_vectorizer, char_vectorizer, data: pd.DataFrame, vec_length=200):
-    word_tokenizer = word_vectorizer.build_tokenizer()
+def create_dataset_generator(word_vectorizer, char_vectorizer, data: pd.DataFrame, one_hot=False, vec_length=200):
+    assert word_vectorizer is not None or char_vectorizer is not None
+
+    if word_vectorizer is not None:
+        word_tokenizer = word_vectorizer.build_tokenizer()
 
     def gen():
         for row in data.iterrows():
+            out_dict = dict()
+
             url = row[1].url
             _type = row[1].type
+            if one_hot:
+                target = tf.squeeze(tf.one_hot([_type], depth=2))
+            else:
+                target = tf.squeeze(_type)
 
-            wv = tf.constant(postpad_to(
-                [[word_vectorizer.vocabulary_.get(a, -1) + 2 for a in word_tokenizer(url)]]  # 0 = padding, 1 = OOV
-                , vec_length), name='word')
-            cv = tf.constant(postpad_to(char_vectorizer.texts_to_sequences([url]), vec_length), name='char')
-            target = _type  # tf.one_hot([_type], depth=2)
+            if word_vectorizer is not None:
+                wv = tf.constant(postpad_to(
+                    [[word_vectorizer.vocabulary_.get(a, -1) + 2 for a in word_tokenizer(url)]]  # 0 = padding, 1 = OOV
+                    , vec_length), name='word')
+                out_dict['word'] = tf.squeeze(wv)
 
-            yield {'word': tf.squeeze(wv), 'char': tf.squeeze(cv)}, tf.squeeze(target)
+            if char_vectorizer is not None:
+                cv = tf.constant(postpad_to(char_vectorizer.texts_to_sequences([url]), vec_length), name='char')
+                out_dict['char'] = tf.squeeze(cv)
 
-    ds = tf.data.Dataset.from_generator(gen, output_types=(
-        {'word': tf.float64, 'char': tf.float64}, tf.int32
-    ), output_shapes=(
-        {'word': tf.TensorShape([vec_length]), 'char': tf.TensorShape([vec_length])}, tf.TensorShape([])
-    ))
+            yield out_dict, target
+
+    output_types, output_shapes = dict(), dict()
+    if word_vectorizer is not None:
+        output_types['word'] = tf.float64
+        output_shapes['word'] = tf.TensorShape([vec_length])
+    if char_vectorizer is not None:
+        output_types['char'] = tf.float64
+        output_shapes['char'] = tf.TensorShape([vec_length])
+
+    ds = tf.data.Dataset.from_generator(
+        gen,
+        output_types=(output_types, tf.int32),
+        output_shapes=(output_shapes, tf.TensorShape([] if not one_hot else [2]))
+    )
 
     return ds
 
